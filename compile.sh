afe471a5af42ccd55e517f66097c606143b4d96a
Nichita Ciuprin <nichitaciuprin@gmail.com>
2019-04-02 16:21:37 -0700

    refactor

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-npm install
+#npm install
 
 rmdir ./dist
 mkdir ./dist
@@ -6,4 +6,3 @@ cp ./src/index.html ./dist
 
 ./node_modules/elm-format/unpacked_bin/elm-format.exe src/ --yes
 ./node_modules/elm/Elm-Platform/0.18.0/.cabal-sandbox/bin/elm-make.exe src/Main.elm --output=./dist/elm.js --yes
-read -r
